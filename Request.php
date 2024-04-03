d332b5816e24d19037adb9264b9c4bd54b02a33a
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <123292918+treptowkolleg@users.noreply.github.com>
2024-04-03 07:40:48 +0200

    Initial commit

---
+++ Request.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the Vaganca project.
+ *
+ * (c) Benjamin Wagner <wagner@vaganca.de>
+ *
+ * For the full copyright and license information, please view the LICENSE.md file that was distributed with this source code.
+ */
+
+namespace Vaganca\Component\HttpTools;
+
+class Request
+{
+
+    public string $request;
+
+    public string $query;
+
+    public string $server;
+
+    public string $files;
+
+    public string $cookies;
+
+    public string $header;
+
+
+}
