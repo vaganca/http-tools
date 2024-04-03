d12affc0349bcb14b2907130a8fb4a72c46e7aa7
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <123292918+treptowkolleg@users.noreply.github.com>
2024-04-03 07:57:07 +0200

    add string request test

---
+++ Tests/Request/string.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * This file is part of the Vaganca project.
+ *
+ * (c) Benjamin Wagner <wagner@vaganca.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+
