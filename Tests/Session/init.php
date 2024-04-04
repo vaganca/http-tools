1234feff9e047bc04af2d9a8391481fc610cc67a
Kollegvertretung der Lernenden am Treptow-Kolleg Berlin <123292918+treptowkolleg@users.noreply.github.com>
2024-04-04 10:53:53 +0200

    add session init test

---
+++ Tests/Session/init.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the Vaganca project.
+ *
+ * (c) Benjamin Wagner <wagner@vaganca.de>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+use Vaganca\Component\HttpTools\Session\Session;
+
+require '../../vendor/autoload.php';
+
+$session = null;
+
+try {
+    $session = new Session();
+    $session->start();
+} catch (LogicException $exception) {
+    echo $exception->getMessage();
+}
+
+echo "User heißt: " .$session->get('user') . PHP_EOL;
+
+$session->set('user','Ben');
